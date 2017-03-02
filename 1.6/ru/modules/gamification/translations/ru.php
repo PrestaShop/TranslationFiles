d5e25c0746431c254b3e4c940ed765a6bb2a6507
Jarvis <jarvis@prestashop.com>
2017-03-02 10:02:29 +0100

    :new: Update 1.6 languages files

--- 1.6/ru/modules/gamification/translations/ru.php
+++ 1.6/ru/modules/gamification/translations/ru.php
@@ -8,7 +8,7 @@ $_MODULE['<{gamification}prestashop>admingamificationcontroller_7258e7251413465e
 $_MODULE['<{gamification}prestashop>admingamificationcontroller_3d4aafb2eedeba2fbf92e852f0af745a'] = 'Торговое мастерство';
 $_MODULE['<{gamification}prestashop>admingamificationcontroller_98f770b0af18ca763421bac22b4b6805'] = 'Характеристики';
 $_MODULE['<{gamification}prestashop>admingamificationcontroller_f5c7922da355fd289ec1d6469e0583a7'] = 'Эффективность продаж';
-$_MODULE['<{gamification}prestashop>admingamificationcontroller_8189ecf686157db0c0274c1f49373318'] = 'Международное присутствие';
+$_MODULE['<{gamification}prestashop>admingamificationcontroller_8189ecf686157db0c0274c1f49373318'] = 'Интернационализация';
 $_MODULE['<{gamification}prestashop>admingamificationcontroller_851f12a0c936baace7f0e734d5c624e7'] = '1. Новичок';
 $_MODULE['<{gamification}prestashop>admingamificationcontroller_583981a16ea761fe852b64094d8a887e'] = '2. Профи';
 $_MODULE['<{gamification}prestashop>admingamificationcontroller_38f7af7416ffcd1524d8a4acda756cbf'] = '3. Эксперт';
