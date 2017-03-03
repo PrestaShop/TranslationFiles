c22b2f7f53dce13be3e0cfcaf554f95e7885abf2
Jarvis <jarvis@prestashop.com>
2017-03-03 10:03:28 +0100

    :new: Update 1.6 languages files

--- 1.6/uk/modules/ganalytics/translations/uk.php
+++ 1.6/uk/modules/ganalytics/translations/uk.php
@@ -10,8 +10,6 @@ $_MODULE['<{ganalytics}prestashop>ganalytics_f4f70727dc34561dfde1a3c529b6205c'] 
 $_MODULE['<{ganalytics}prestashop>ganalytics_851bd83a102d143ee17d97f9e15e15af'] = 'ID-номер для відстеження Google Аналітики';
 $_MODULE['<{ganalytics}prestashop>ganalytics_ad8e83a47926dcb12e8a8fccd7fcf604'] = 'Ця інформація доступна у Вашому профілі Google Анатлітика';
 $_MODULE['<{ganalytics}prestashop>ganalytics_bcd08e73ca9d8bfed2cccd50dd6d8328'] = 'Включити відстеження ID клієнта';
-$_MODULE['<{ganalytics}prestashop>ganalytics_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Вкл';
-$_MODULE['<{ganalytics}prestashop>ganalytics_b9f5c797ebbf55adccdd8539a65a0241'] = 'Викл';
 $_MODULE['<{ganalytics}prestashop>ganalytics_612e1a524ec979815b12182c34e2daa0'] = 'ID профілю успішно оновлено';
 $_MODULE['<{ganalytics}prestashop>configuration_df15c5cf264eb769b087f9d81aff029f'] = 'З функціональністю електронної комерції в Google Аналітиці ви можете отримати чітке уявлення про важливі показники поведінки покупців та конверсії, в тому числі:';
 $_MODULE['<{ganalytics}prestashop>configuration_ade6a18bb6c147db87bc9463240e455a'] = 'Дії "Додати в кошик"';
