387a9cb4753415b8428be60ec75f9bc5821124c0
Jarvis <jarvis@prestashop.com>
2017-04-06 10:02:31 +0200

    :new: Update 1.6 languages files

--- 1.6/ja/modules/blocktopmenu/translations/ja.php
+++ 1.6/ja/modules/blocktopmenu/translations/ja.php
@@ -28,6 +28,7 @@ $_MODULE['<{blocktopmenu}prestashop>blocktopmenu_e93c33bd1341ab74195430daeb63db1
 $_MODULE['<{blocktopmenu}prestashop>blocktopmenu_387a8014f530f080bf2f3be723f8c164'] = 'リンクリスト';
 $_MODULE['<{blocktopmenu}prestashop>form_1063e38cb53d94d386f21227fcd84717'] = '削除';
 $_MODULE['<{blocktopmenu}prestashop>form_ec211f7c20af43e742bf2570c3cb84f9'] = '追加';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_b61541208db7fa7dba42c85224405911'] = 'メニュー';
 
 
 return $_MODULE;
--- 1.6/ja/modules/gamification/translations/ja.php
+++ 1.6/ja/modules/gamification/translations/ja.php
@@ -5,6 +5,7 @@ $_MODULE = array();
 
 $_MODULE['<{gamification}prestashop>admingamificationcontroller_7258e7251413465e0a3eb58094430bde'] = '管理';
 $_MODULE['<{gamification}prestashop>admingamificationcontroller_98f770b0af18ca763421bac22b4b6805'] = '機能';
+$_MODULE['<{gamification}prestashop>admingamificationcontroller_8189ecf686157db0c0274c1f49373318'] = 'インターナショナル';
 $_MODULE['<{gamification}prestashop>filters_bt_b1c94ca2fbc3e78fc30069c8d0f01680'] = 'すべて';
 $_MODULE['<{gamification}prestashop>filters_b1c94ca2fbc3e78fc30069c8d0f01680'] = 'すべて';
 $_MODULE['<{gamification}prestashop>view_bt_ce8ae9da5b7cd6c3df2929543a9af92d'] = 'メール';
