ea6fb3c06b16d994489ffdbc7ba344b0ddd50ede
Jarvis <jarvis@prestashop.com>
2017-04-23 10:03:20 +0200

    :new: Update 1.6 languages files

--- 1.6/tw/modules/productscategory/translations/tw.php
+++ 1.6/tw/modules/productscategory/translations/tw.php
@@ -3,7 +3,7 @@
 global $_MODULE;
 $_MODULE = array();
 
-$_MODULE['<{productscategory}prestashop>productscategory_8a4f5a66d0fcc9d13614516db6e3d47a'] = '同一分類中的商品';
+$_MODULE['<{productscategory}prestashop>productscategory_8a4f5a66d0fcc9d13614516db6e3d47a'] = '同類商品';
 $_MODULE['<{productscategory}prestashop>productscategory_1d269d7f013c3d9d891a146f4379eb02'] = '在商品頁面上添加顯示相同分類商品頁面的區塊。';
 $_MODULE['<{productscategory}prestashop>productscategory_8dd2f915acf4ec98006d11c9a4b0945b'] = '已成功更新設定。';
 $_MODULE['<{productscategory}prestashop>productscategory_f4f70727dc34561dfde1a3c529b6205c'] = '設定';
@@ -12,8 +12,8 @@ $_MODULE['<{productscategory}prestashop>productscategory_1d986024f548d57b1d743ec
 $_MODULE['<{productscategory}prestashop>productscategory_00d23a76e43b46dae9ec7aa9dcbebb32'] = '啟用';
 $_MODULE['<{productscategory}prestashop>productscategory_b9f5c797ebbf55adccdd8539a65a0241'] = '停用';
 $_MODULE['<{productscategory}prestashop>productscategory_c9cc8cce247e49bae79f15173ce97354'] = '儲存';
-$_MODULE['<{productscategory}prestashop>productscategory_f55e0a28b86c2ab66ac632ab9ddf1833'] = '同一分類內有%s個其他商品：';
-$_MODULE['<{productscategory}prestashop>productscategory_bebb44f38b03407098d48198c1d0aaa5'] = '同一分類內有%s個其他商品：';
+$_MODULE['<{productscategory}prestashop>productscategory_f55e0a28b86c2ab66ac632ab9ddf1833'] = '%s 個同類商品：';
+$_MODULE['<{productscategory}prestashop>productscategory_bebb44f38b03407098d48198c1d0aaa5'] = '%s 個同類商品：';
 $_MODULE['<{productscategory}prestashop>productscategory_dd1f775e443ff3b9a89270713580a51b'] = '上一步';
 $_MODULE['<{productscategory}prestashop>productscategory_10ac3d04253ef7e1ddc73e6091c0cd55'] = '下一步';
 $_MODULE['<{productscategory}prestashop>productscategory_2d0f6b8300be19cf35e89e66f0677f95'] = '加入購物車';
