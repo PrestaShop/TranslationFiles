ca902051a87e6a4ee36e1104e6d418ca71184238
Jarvis <jarvis@prestashop.com>
2017-04-26 10:02:36 +0200

    :new: Update 1.6 languages files

--- 1.6/ko/modules/watermark/translations/ko.php
+++ 1.6/ko/modules/watermark/translations/ko.php
@@ -4,8 +4,6 @@ global $_MODULE;
 $_MODULE = array();
 
 $_MODULE['<{watermark}prestashop>watermark_f4f70727dc34561dfde1a3c529b6205c'] = '설정';
-$_MODULE['<{watermark}prestashop>watermark_00d23a76e43b46dae9ec7aa9dcbebb32'] = '사용';
-$_MODULE['<{watermark}prestashop>watermark_b9f5c797ebbf55adccdd8539a65a0241'] = '사용안함';
 $_MODULE['<{watermark}prestashop>watermark_c9cc8cce247e49bae79f15173ce97354'] = '저장';
 
 
