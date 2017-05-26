baa88de8f03943dc77a1beff52e130641265e040
Jarvis <jarvis@prestashop.com>
2017-05-26 10:02:35 +0200

    :new: Update 1.6 languages files

--- 1.6/ko/modules/productscategory/translations/ko.php
+++ 1.6/ko/modules/productscategory/translations/ko.php
@@ -4,12 +4,8 @@ global $_MODULE;
 $_MODULE = array();
 
 $_MODULE['<{productscategory}prestashop>productscategory_f4f70727dc34561dfde1a3c529b6205c'] = '설정';
-$_MODULE['<{productscategory}prestashop>productscategory_00d23a76e43b46dae9ec7aa9dcbebb32'] = '사용';
-$_MODULE['<{productscategory}prestashop>productscategory_b9f5c797ebbf55adccdd8539a65a0241'] = '사용안함';
 $_MODULE['<{productscategory}prestashop>productscategory_c9cc8cce247e49bae79f15173ce97354'] = '저장';
 $_MODULE['<{productscategory}prestashop>productscategory_dd1f775e443ff3b9a89270713580a51b'] = '이전';
-$_MODULE['<{productscategory}prestashop>productscategory_10ac3d04253ef7e1ddc73e6091c0cd55'] = 'Suivant';
-$_MODULE['<{productscategory}prestashop>productscategory_2d0f6b8300be19cf35e89e66f0677f95'] = '장바구니 담기';
 
 
 return $_MODULE;
--- 1.6/ko/modules/statsdata/translations/ko.php
+++ 1.6/ko/modules/statsdata/translations/ko.php
@@ -3,10 +3,8 @@
 global $_MODULE;
 $_MODULE = array();
 
-$_MODULE['<{statsdata}prestashop>statsdata_c77dfd683d0d76940e5e04cb24e8bce1'] = '통계';
+$_MODULE['<{statsdata}prestashop>statsdata_c77dfd683d0d76940e5e04cb24e8bce1'] = '이 모듈은 통계를 사용하기 원하면 활성화해야 합니다.';
 $_MODULE['<{statsdata}prestashop>statsdata_f4f70727dc34561dfde1a3c529b6205c'] = '설정';
-$_MODULE['<{statsdata}prestashop>statsdata_00d23a76e43b46dae9ec7aa9dcbebb32'] = '사용';
-$_MODULE['<{statsdata}prestashop>statsdata_b9f5c797ebbf55adccdd8539a65a0241'] = '사용안함';
 $_MODULE['<{statsdata}prestashop>statsdata_c9cc8cce247e49bae79f15173ce97354'] = '저장';
 
 
