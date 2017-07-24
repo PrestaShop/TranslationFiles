67d81b452e4ca2a57378dd0d7c3638fd831da60c
Jarvis <jarvis@prestashop.com>
2017-07-24 10:02:44 +0200

    :new: Update 1.6 languages files

--- 1.6/ko/translations/ko/pdf.php
+++ 1.6/ko/translations/ko/pdf.php
@@ -9,24 +9,17 @@ $_LANGPDF['PDF466eadd40b3c10580e3ab4e8061161ce'] = '청구서';
 $_LANGPDF['PDFcc894fffb019b528d12951b74d6d2b6e'] = '주문 반환';
 $_LANGPDF['PDFcf3bae95c5f6023d5a10fe415b205a45'] = '신용전표';
 $_LANGPDF['PDFf771e4a4df40bf061607fdb8e3f140dc'] = '공급 주문서';
-$_LANGPDF['PDF63d5049791d9d79d86e9a108b0a999ca'] = '참고';
 $_LANGPDF['PDFdeb10517653c255364175796ace3553f'] = '상품';
 $_LANGPDF['PDF03ab340b3f99e03cff9e84314ead38c0'] = '수량';
 $_LANGPDF['PDF914419aa32f04011357d3b604a86d7eb'] = '배송업체';
 $_LANGPDF['PDFbe53a0541a6d36f6ecb879fa2c584b08'] = '이미지';
 $_LANGPDF['PDF104d9898c04874d0fbac36e125fa1369'] = '할인';
 $_LANGPDF['PDF96b0141273eabab320119c467cdcaf17'] = '합계';
-$_LANGPDF['PDF3dd42bfc8345e82c43b4a40a3426ec49'] = '참조';
 $_LANGPDF['PDF9d5bf15117441a1b52eb1f0808e4aad3'] = '할인';
 $_LANGPDF['PDFea9cf7e47ff33b2be14e6dd07cbcefc6'] = '배송';
 $_LANGPDF['PDFba794350deb07c0c96fe73bd12239059'] = '포장';
-$_LANGPDF['PDF88be9ea838e21273267409d76af3b284'] = '고객님의 반품 요구가 접수 되었습니다.';
-$_LANGPDF['PDFdd0b8feb4eb6c389c284518f5683017c'] = '소포가 우리에게 도착해야합니다';
-$_LANGPDF['PDFea62a2e28a800c367509773730120a67'] = '귀하의 주문이 접수되었습니다';
-$_LANGPDF['PDF44749712dbec183e983dcd78a7736c41'] = '날짜';
-$_LANGPDF['PDF6c957f72dc8cdacc75762f2cbdcdfaf2'] = '단가';
 $_LANGPDF['PDF3601146c4e948c32b6424d2c0a7f0118'] = '가격';
-$_LANGPDF['PDF716be2c7de31f063369ca297a9928403'] = '세금';
+$_LANGPDF['PDF716be2c7de31f063369ca297a9928403'] = '세금:';
 
 
 return $_LANGPDF;
