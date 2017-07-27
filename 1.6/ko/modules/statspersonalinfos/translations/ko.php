39d43308eb3645a2ee41851ed72dadd1dae88fa6
Jarvis <jarvis@prestashop.com>
2017-07-27 10:02:46 +0200

    :new: Update 1.6 languages files

--- 1.6/ko/modules/statspersonalinfos/translations/ko.php
+++ 1.6/ko/modules/statspersonalinfos/translations/ko.php
@@ -4,7 +4,6 @@ global $_MODULE;
 $_MODULE = array();
 
 $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_6602bbeb2956c035fb4cb5e844a4861b'] = '안내서';
-$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_998e4c5c80f27dec552e99dfed34889a'] = 'csv로 내보내기';
 $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_63889cfb9d3cbe05d1bd2be5cc9953fd'] = '남성';
 $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_b719ce180ec7bd9641fece2f920f4817'] = '여성';
 $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_88183b946cc5f0e8c96b2e66e1c74a7e'] = '알수 없음';
