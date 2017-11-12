c375eb44863e23302743d52f3f26a12d17b77922
Jarvis <jarvis@prestashop.com>
2017-11-12 10:02:41 +0100

    :new: Update 1.6 languages files

--- 1.6/he/modules/ganalytics/translations/he.php
+++ 1.6/he/modules/ganalytics/translations/he.php
@@ -8,6 +8,7 @@ $_MODULE['<{ganalytics}prestashop>ganalytics_630f6dc397fe74e52d5189e2c80f282b'] 
 $_MODULE['<{ganalytics}prestashop>ganalytics_f4f70727dc34561dfde1a3c529b6205c'] = 'הגדרות';
 $_MODULE['<{ganalytics}prestashop>ganalytics_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'מופעל';
 $_MODULE['<{ganalytics}prestashop>ganalytics_b9f5c797ebbf55adccdd8539a65a0241'] = 'מנוטרל';
+$_MODULE['<{ganalytics}prestashop>configuration_613c191b4a1f82a454b72a840d96eefd'] = 'פרטי מוצר';
 $_MODULE['<{ganalytics}prestashop>form-ps14_c9cc8cce247e49bae79f15173ce97354'] = 'שמור';
 
 
