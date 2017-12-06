b1593111032aa8efe3a27b8f0ddc5ca37199b59b
Jarvis <jarvis@prestashop.com>
2017-12-06 10:04:13 +0100

    :new: Update 1.6 languages files

--- 1.6/ko/modules/productpaymentlogos/translations/ko.php
+++ 1.6/ko/modules/productpaymentlogos/translations/ko.php
@@ -3,8 +3,16 @@
 global $_MODULE;
 $_MODULE = array();
 
+$_MODULE['<{productpaymentlogos}prestashop>productpaymentlogos_1056d03db619b016d8fc6b60d08ef488'] = '상품 결제 로고 블럭';
+$_MODULE['<{productpaymentlogos}prestashop>productpaymentlogos_88fd6d994459806f2dcb8999ac03c76e'] = '상품 페이지에 사용 가능한 결제 시스템의 로고를 보여줍니다.';
+$_MODULE['<{productpaymentlogos}prestashop>productpaymentlogos_126b21ce46c39d12c24058791a236777'] = '잘못된 이미지';
+$_MODULE['<{productpaymentlogos}prestashop>productpaymentlogos_df7859ac16e724c9b1fba0a364503d72'] = '파일을 업로드하는 동안 오류가 발생했습니다.';
 $_MODULE['<{productpaymentlogos}prestashop>productpaymentlogos_f4f70727dc34561dfde1a3c529b6205c'] = '설정';
+$_MODULE['<{productpaymentlogos}prestashop>productpaymentlogos_223795d3a336ef80c5b6a070ae4e0d2a'] = '블럭 머리말';
+$_MODULE['<{productpaymentlogos}prestashop>productpaymentlogos_c7dd01cfb61ff8c984c0aff44f6540e3'] = '로고 위에 머리말을 추가하기 위해 선택할 수 있습니다.';
+$_MODULE['<{productpaymentlogos}prestashop>productpaymentlogos_89ca5c48bbc6b7a648a5c1996767484c'] = '블럭 이미지';
 $_MODULE['<{productpaymentlogos}prestashop>productpaymentlogos_9ce38727cff004a058021a6c7351a74a'] = '이미지 링크';
+$_MODULE['<{productpaymentlogos}prestashop>productpaymentlogos_826eeee52fe142372c3a2bc195eff911'] = '상단의 양식을 사용하여 자체 이미지를 업로드 하던가, "이미지 링크" 옵션에서 링크를 걸 수 있습니다.';
 $_MODULE['<{productpaymentlogos}prestashop>productpaymentlogos_c9cc8cce247e49bae79f15173ce97354'] = '저장';
 
 
