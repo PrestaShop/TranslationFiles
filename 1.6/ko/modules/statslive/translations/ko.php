622da8f891fbb93ad3c169ad19e25526ac1872eb
Jarvis <jarvis@prestashop.com>
2017-12-28 10:04:29 +0100

    :new: Update 1.6 languages files

--- 1.6/ko/modules/statslive/translations/ko.php
+++ 1.6/ko/modules/statslive/translations/ko.php
@@ -5,6 +5,7 @@ $_MODULE = array();
 
 $_MODULE['<{statslive}prestashop>statslive_fa55230e9791f2b71322869318a5f00f'] = '온라인 방문자';
 $_MODULE['<{statslive}prestashop>statslive_66c4c5112f455a19afde47829df363fa'] = '합계:';
+$_MODULE['<{statslive}prestashop>statslive_d37c2bf1bd3143847fca087b354f920e'] = '고객 ID';
 $_MODULE['<{statslive}prestashop>statslive_49ee3087348e8d44e1feda1917443987'] = '이름';
 $_MODULE['<{statslive}prestashop>statslive_a12a3079e14ced46e69ba52b8a90b21a'] = 'IP';
 $_MODULE['<{statslive}prestashop>statslive_b6f05e5ddde1ec63d992d61144452dfa'] = '리퍼러';
