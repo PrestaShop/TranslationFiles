20253d752e6c597db2697ef196bdc444a7f88214
Jarvis <jarvis@prestashop.com>
2017-12-30 10:04:06 +0100

    :new: Update 1.6 languages files

--- 1.6/bs/translations/bs/admin.php
+++ 1.6/bs/translations/bs/admin.php
@@ -3,6 +3,9 @@
 global $_LANGADM;
 $_LANGADM = array();
 
+$_LANGADM['AdminShope93c33bd1341ab74195430daeb63db13'] = 'Naziv trgovine';
+$_LANGADM['AdminShopUrle93c33bd1341ab74195430daeb63db13'] = 'Naziv trgovine';
+$_LANGADM['AdminStorese93c33bd1341ab74195430daeb63db13'] = 'Naziv trgovine';
 $_LANGADM['AdminControllere6a7f8a2f42cc35979973da8dfb10720'] = 'Forum';
 
 
