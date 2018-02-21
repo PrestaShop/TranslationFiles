6c0fd3541ab6ba27f380bd0cd7285d7139363cfb
Jarvis <jarvis@prestashop.com>
2018-02-21 10:02:52 +0100

    :new: Update 1.6 languages files

--- 1.6/hi/modules/autoupgrade/translations/hi.php
+++ 1.6/hi/modules/autoupgrade/translations/hi.php
@@ -4,6 +4,7 @@ global $_MODULE;
 $_MODULE = array();
 
 $_MODULE['<{autoupgrade}prestashop>adminpreferences_6adf97f83acf6453d4a6a4b1070f3754'] = 'कुछ नहीं';
+$_MODULE['<{autoupgrade}prestashop>adminselfupgrade_801ab24683a4a8c433c6eb40c48bcd9d'] = 'डाउनलोड';
 
 
 return $_MODULE;
--- 1.6/hi/themes/default-bootstrap/lang/hi.php
+++ 1.6/hi/themes/default-bootstrap/lang/hi.php
@@ -4,6 +4,7 @@ global $_LANG;
 $_LANG = array();
 
 $_LANG['discount_6adf97f83acf6453d4a6a4b1070f3754'] = 'कुछ नहीं';
+$_LANG['product_801ab24683a4a8c433c6eb40c48bcd9d'] = 'डाउनलोड';
 $_LANG['sitemap_453aceb005ceaf54a47da15fee8b2a26'] = 'पृष्ठ';
 
 
