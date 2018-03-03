f7e9f7e34f12bb2bbf8b4d05d3b979f574cb01fc
Jarvis <jarvis@prestashop.com>
2018-03-03 10:02:40 +0100

    :new: Update 1.6 languages files

--- 1.6/sq/modules/statscarrier/translations/sq.php
+++ 1.6/sq/modules/statscarrier/translations/sq.php
@@ -3,7 +3,6 @@
 global $_MODULE;
 $_MODULE = array();
 
-$_MODULE['<{statscarrier}prestashop>statscarrier_2e6774abc54cb13cef2c5bfd5a2cb463'] = 'Shpërndarja e trasnportuesëve';
 $_MODULE['<{statscarrier}prestashop>statscarrier_b1c94ca2fbc3e78fc30069c8d0f01680'] = 'Të gjitha';
 
 
