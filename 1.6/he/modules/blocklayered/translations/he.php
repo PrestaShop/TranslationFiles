4652289b082c3764b63a1b81942fb20233f3ecb8
Jarvis <jarvis@prestashop.com>
2018-04-14 10:03:09 +0200

    :new: Update 1.6 languages files

--- 1.6/he/modules/blocklayered/translations/he.php
+++ 1.6/he/modules/blocklayered/translations/he.php
@@ -205,6 +205,7 @@ $_MODULE['<{blocklayered}prestashop>view_8531c73de81b9ed94322dda7cf947daa'] = '�
 $_MODULE['<{blocklayered}prestashop>view_ee61c015043c79c1370fc14980dd27b9'] = 'הראה מוצרים מתוך תת-קטגוריות';
 $_MODULE['<{blocklayered}prestashop>view_a19399fa42f1ab059401a14b9f13eba1'] = 'עומק מסנן קטגוריות ( 0 - ללא מגבלה, 1 כברירת מחדל)';
 $_MODULE['<{blocklayered}prestashop>view_3e652bd299bb3ee3d458c0dcc7fd706e'] = 'השתמש במס לסנון מחיר';
+$_MODULE['<{blocklayered}prestashop>view_30b1f6f4369e3d0f7a0d50b5cb96aabd'] = 'השתמש בעיגול לסינון מחיר';
 $_MODULE['<{blocklayered}prestashop>view_bb17448782eea2b49a97deac234e9851'] = 'אפשר לרובטי מפתוח (גוגל, יאהו, בינג, וכו\') להשתמש במסנן \'מצב\'';
 $_MODULE['<{blocklayered}prestashop>view_affe7250d1c6cfb3ac0dd054376d4b55'] = 'אפשר לרובטי מפתוח (גוגל, יאהו, בינג, וכו\') להשתמש במסנן \'זמינות\'';
 $_MODULE['<{blocklayered}prestashop>view_e1d2fdc12d2e2303a2853e2ba4ff6524'] = 'אפשר לרובטי מפתוח (גוגל, יאהו, בינג, וכו\') להשתמש במסנן \'יצרן\'';
