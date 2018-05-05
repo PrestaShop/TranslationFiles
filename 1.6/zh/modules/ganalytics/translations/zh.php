430bb0d2b071c4afe7bfbfad763de3c6a324896e
Jarvis <jarvis@prestashop.com>
2018-05-05 10:04:07 +0200

    :new: Update 1.6 languages files

--- 1.6/zh/modules/ganalytics/translations/zh.php
+++ 1.6/zh/modules/ganalytics/translations/zh.php
@@ -4,12 +4,19 @@ global $_MODULE;
 $_MODULE = array();
 
 $_MODULE['<{ganalytics}prestashop>ganalytics_d86cf69a8b82547a94ca3f6a307cf9a6'] = 'Google Analytics';
+$_MODULE['<{ganalytics}prestashop>ganalytics_7ed5c154078e30b30b2f214299c5e9f5'] = '您确定要卸载Google Analytics吗？ 你会失去所有与这个模块有关的数据。';
 $_MODULE['<{ganalytics}prestashop>ganalytics_c9cc8cce247e49bae79f15173ce97354'] = '保存';
 $_MODULE['<{ganalytics}prestashop>ganalytics_630f6dc397fe74e52d5189e2c80f282b'] = '返回列表　';
 $_MODULE['<{ganalytics}prestashop>ganalytics_f4f70727dc34561dfde1a3c529b6205c'] = '设置';
 $_MODULE['<{ganalytics}prestashop>ganalytics_851bd83a102d143ee17d97f9e15e15af'] = 'Google Analytics Tracking ID';
 $_MODULE['<{ganalytics}prestashop>ganalytics_00d23a76e43b46dae9ec7aa9dcbebb32'] = '启用';
 $_MODULE['<{ganalytics}prestashop>ganalytics_b9f5c797ebbf55adccdd8539a65a0241'] = '禁用';
+$_MODULE['<{ganalytics}prestashop>ganalytics_8e7b67b44506b7ac6685da041a603044'] = '用户ID设置已成功更新';
+$_MODULE['<{ganalytics}prestashop>configuration_df15c5cf264eb769b087f9d81aff029f'] = '借助Google Analytics中的电子商务功能，您可以清楚地了解有关购物者行为和转化的重要指标，其中包括：';
+$_MODULE['<{ganalytics}prestashop>configuration_613c191b4a1f82a454b72a840d96eefd'] = '产品细节视图';
+$_MODULE['<{ganalytics}prestashop>configuration_ade6a18bb6c147db87bc9463240e455a'] = '“添加到购物车”操作';
+$_MODULE['<{ganalytics}prestashop>configuration_33f50b54625790316b86485ff3e794c4'] = '内部竞选点击';
+$_MODULE['<{ganalytics}prestashop>configuration_16fafb5cce24f766bf2c8fcebecf76fc'] = '创建您的帐户以开始。';
 $_MODULE['<{ganalytics}prestashop>form-ps14_c9cc8cce247e49bae79f15173ce97354'] = '保存';
 
 
