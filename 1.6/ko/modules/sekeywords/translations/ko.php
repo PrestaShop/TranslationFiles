b2a9d377c0b7c8d73edb583f70d51f019f7ef0b6
Jarvis <jarvis@prestashop.com>
2018-06-19 10:04:28 +0200

    :new: Update 1.6 languages files

--- 1.6/ko/modules/sekeywords/translations/ko.php
+++ 1.6/ko/modules/sekeywords/translations/ko.php
@@ -5,6 +5,7 @@ $_MODULE = array();
 
 $_MODULE['<{sekeywords}prestashop>sekeywords_65b0b42febc8ea16db4652eab6f420a4'] = '검색 엔진 키워드';
 $_MODULE['<{sekeywords}prestashop>sekeywords_6602bbeb2956c035fb4cb5e844a4861b'] = '안내서';
+$_MODULE['<{sekeywords}prestashop>sekeywords_dcbcf5d190af87351a16edd5f132c657'] = '중요 사항: 2013년 9월, Google은 SSL을 사용하여 검색어를 암호화하기로 결정했습니다. 이는 전 세계의 모든 리퍼러 기반 도구 (이 도구 포함) 가 Google 키워드를 더 이상 식별 할 수 없음을 의미합니다.';
 $_MODULE['<{sekeywords}prestashop>sekeywords_9639e32cab248434a17ab32237cb3b71'] = '적용';
 $_MODULE['<{sekeywords}prestashop>sekeywords_867343577fa1f33caa632a19543bd252'] = '키워드';
 $_MODULE['<{sekeywords}prestashop>sekeywords_998e4c5c80f27dec552e99dfed34889a'] = 'CSV로 내보내기';
