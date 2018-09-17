df650c054fffb340cbc9803da17f3964bffcf020
Jarvis <jarvis@prestashop.com>
2018-09-17 10:04:05 +0200

    :new: Update 1.6 languages files

--- 1.6/cs/modules/blockcontactinfos/translations/cs.php
+++ 1.6/cs/modules/blockcontactinfos/translations/cs.php
@@ -15,7 +15,7 @@ $_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_c9cc8cce247e49bae79f1
 $_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_02d4482d332e1aef3437cd61c9bcc624'] = 'Napište nám';
 $_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_2e006b735fbd916d8ab26978ae6714d4'] = 'Telefon';
 $_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_6a1e265f92087bb6dd18194833fe946b'] = 'E-mail:';
-$_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_80a11d2a54a677f6fadd9c041c0d6b98'] = 'Kontakt';
+$_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_80a11d2a54a677f6fadd9c041c0d6b98'] = 'Informace o obchodu';
 $_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_320abee94a07e976991e4df0d4afb319'] = 'Zavolejte nám:';
 
 
