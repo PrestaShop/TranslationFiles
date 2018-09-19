a73a2df9b09708966a913ed3abb82a8e0da8dc9d
Jarvis <jarvis@prestashop.com>
2018-09-19 10:04:57 +0200

    :new: Update 1.6 languages files

--- 1.6/ko/modules/statspersonalinfos/translations/ko.php
+++ 1.6/ko/modules/statspersonalinfos/translations/ko.php
@@ -3,8 +3,25 @@
 global $_MODULE;
 $_MODULE = array();
 
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_1b94af23fd8ea879910a6307460ebba4'] = '등록된 고객 정보';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_b0cb4ea4e89c2107cb100be8417fd94e'] = '통계 대시보드에 등록된 고객에 대한 정보(성별이나 나이 등) 를 추가합니다.';
 $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_6602bbeb2956c035fb4cb5e844a4861b'] = '안내서';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_3ca26e413e485538a7f3e610c0324636'] = '목표 고객';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_74036f0101d2f3f9b082b8de981c18dc'] = '각 메시지가 영향을 미치기 위해서는, 메시지가 누구에게 전달되는지 알아야합니다. ';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_daa0ad6a8b07a64f18b26e0f5e1255bd'] = '목표 고객 정의는 그들을 끌어들이려 적당한 도구를 선택하는데 필수적입니다.';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_8044a3d0d61282c4c425cd0da5432f11'] = '작업을 클라이언트의 그룹 -- 또는 그룹들-- 으로 제한하는 것이 가장 좋습니다.';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_05f074d112460c990203ff2f17a7d46d'] = '등록 된 고객 정보를 저장하면 고객 프로파일을 정확하게 정의 할 수 있으므로 특별한 판매나 프로모션에 적용 할 수 있습니다.';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_9dc739320e2523183e8b31f395e1e489'] = '다음과 같은 방식으로 판매를 늘릴 수 있습니다:';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_2f619981f1abd95da0907766cc409be9'] = '타겟 광고 캠페인 시작합니다.';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_428dc481e13dad2818d8088da7362dd2'] = '클라이언트 그룹에 이메일이나 소식지로 연락합니다.';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_12344d957b4aee1e2c322549ddc9741f'] = '성별 분포를 통해 상점의 남성과 여성 구매자의 비율을 결정할 수 있습니다.';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_998e4c5c80f27dec552e99dfed34889a'] = 'CSV로 내보내기';
 $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_08a12630390dc8ddd4c9fdcd9060dce6'] = '연령 범위는 대상의 인구통계를 더 잘 이해하게 합니다.';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_db9750afee08c6e88c91fbe1b3404ba6'] = '국가 분포는 당신의 고객이 어느 국가에서 구매하는지 분석하게 해 줍니다.';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_e0fcbbd416a4872ec8f91fefa823debe'] = '통화 범위는 당신의 고객이 사용하는 통화를 결정하게 해 줍니다.';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_94d50571d0f2e98276cfbe2f3de41b20'] = '언어 분포는 당신의 고객들이 웹 탐색에서 사용하는 언어를 분석하게 해 줍니다.';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_9fde3510abe63111086fd4288a19e1be'] = '아직 등록된 고객이 없습니다.';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_8dc514f6da8c696c4a522efc145ad28a'] = '성별 분포';
 $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_63889cfb9d3cbe05d1bd2be5cc9953fd'] = '남성';
 $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_b719ce180ec7bd9641fece2f920f4817'] = '여성';
 $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_88183b946cc5f0e8c96b2e66e1c74a7e'] = '알 수 없음';
@@ -14,6 +31,10 @@ $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_b20e0ed6158978a3a23
 $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_e5884ca49180d38295ee426c624d936c'] = '25-34';
 $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_62ce9f61153b331eabe9efc2fc7eb5c2'] = '35-49';
 $_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_a7724f78dcdf2179c5ca651d15ed5b2c'] = '50-59';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_6a239f88f1aeb0561e7786b6120d1d5e'] = '60+';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_73b0130037e21b76351aebfd29d0b9aa'] = '국가 분포';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_28e81c8343702f6c813cc31a7f90616a'] = '통화 분포';
+$_MODULE['<{statspersonalinfos}prestashop>statspersonalinfos_f1dd68fb6a00d3e4d7f751deacde995d'] = '언어 분포';
 
 
 return $_MODULE;
