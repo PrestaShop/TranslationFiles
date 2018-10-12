3ce8420bb4e35cafab4dc6c880d6c929396ca74e
Jarvis <jarvis@prestashop.com>
2018-10-12 10:05:36 +0200

    :new: Update 1.6 languages files

--- 1.6/ko/modules/mailalerts/translations/ko.php
+++ 1.6/ko/modules/mailalerts/translations/ko.php
@@ -7,6 +7,9 @@ $_MODULE['<{mailalerts}prestashop>mailalerts-account_d95cf4ab2cbf1dfb63f066b5055
 $_MODULE['<{mailalerts}prestashop>mailalerts-account_4edfd10d0bb5f51e0fd2327df608b5a8'] = '나의 알림';
 $_MODULE['<{mailalerts}prestashop>my-account_4edfd10d0bb5f51e0fd2327df608b5a8'] = '나의 알림';
 $_MODULE['<{mailalerts}prestashop>product_67135a14d3ac4f1369633dd006d6efec'] = 'your@email.com';
+$_MODULE['<{mailalerts}prestashop>product_546e02eaa9a986c83cc347e273269f2c'] = '가능할 때 내게 알리기';
+$_MODULE['<{mailalerts}prestashop>product_61172eb93737ebf095d3fa02119ce1df'] = '요청 알림이 등록됨';
+$_MODULE['<{mailalerts}prestashop>product_900f8551b29793ecb604a545b2059cc1'] = '잘못된 이메일 주소입니다';
 
 
 return $_MODULE;
