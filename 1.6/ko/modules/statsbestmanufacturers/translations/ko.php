32312fa10b63df52409aade3854eb9df0d92b78f
Jarvis <jarvis@prestashop.com>
2018-10-29 10:04:50 +0100

    :new: Update 1.6 languages files

--- 1.6/ko/modules/statsbestmanufacturers/translations/ko.php
+++ 1.6/ko/modules/statsbestmanufacturers/translations/ko.php
@@ -10,6 +10,7 @@ $_MODULE['<{statsbestmanufacturers}prestashop>statsbestmanufacturers_2a0440eec72
 $_MODULE['<{statsbestmanufacturers}prestashop>statsbestmanufacturers_ea067eb37801c5aab1a1c685eb97d601'] = '총 지불합계';
 $_MODULE['<{statsbestmanufacturers}prestashop>statsbestmanufacturers_72fd9b5482201824daae557360d91196'] = '최고 제조사';
 $_MODULE['<{statsbestmanufacturers}prestashop>statsbestmanufacturers_891374571b52a78a29a622308f1fa292'] = '상태 대시보드에 최고의 제조사 목록을 추가합니다.';
+$_MODULE['<{statsbestmanufacturers}prestashop>statsbestmanufacturers_998e4c5c80f27dec552e99dfed34889a'] = 'CSV로 내보내기';
 
 
 return $_MODULE;
